ed152ba1e2b5126d686ed510834550b32e9814da
JonaVS <jon4963@gmail.com>
2023-02-03 16:25:32 -0600

    fix: resolve tag table not being created
    -Using sequelize.sync method doesnt create all tables
    -A call to the sync method of each model is used to resolve the issue

--- src/db/dbInit.ts
+++ src/db/dbInit.ts
@@ -1,9 +1,21 @@
 import { sequelizeConnection } from "./config.js";
+import User from "../models/User.js";
+import Todo from "../models/Todo.js";
+import Tag from "../models/Tag.js";
 
 export const initDbConnection = async (): Promise<boolean> => {
   let success = false;
   try {
-    await sequelizeConnection.sync({alter: true})
+    /*
+      -Using just sequelizeConnection.sync({alter:true}) doesnt create all tables. Weird issue.
+      -Due to the above issue, I decided to use the sync method for each model.
+      -As a reminder, sync methods are only for development. For real things migrations are used.
+    */ 
+    Promise.all([
+      await User.sync({ alter: true }),
+      await Todo.sync({ alter: true }),
+      await Tag.sync({ alter: true }),
+    ]);
     success = true;
     console.log("Database connection and synchronization has been completed successfully");
   } catch (error) {
